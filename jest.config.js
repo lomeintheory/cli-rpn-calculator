67bc351a5557aa9906b7a47dab801b6e33f64b24
Andrew Lo <andrewlo957@gmail.com>
2025-06-28 13:53:01 -0400

    FIX: Change jest config to Commonjs syntax

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest/presets/default-esm',
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   testEnvironment: 'node',
 };
